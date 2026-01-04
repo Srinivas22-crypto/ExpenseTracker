bc68a68e421f1952917f70c0e8620fbc932dedc5
Srinivas22-crypto <potharajusrinivas05@gmail.com>
2026-01-04 22:05:37 +0530

    changed viteconifg

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -13,10 +12,20 @@ export default defineConfig(({ mode }) => ({
       'expensetracker-trro.onrender.com'
     ],
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react({
+      jsxImportSource: 'react',
+      tsDecorators: true,
+    }),
+    mode === "development" && componentTagger()
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    sourcemap: false,
+    minify: 'terser',
   },
 }));
